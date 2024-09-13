fcb1aa0add99b5675ffd5ad0195f14932b178d6a
DoCopyPaste <docopypaste.user@gmail.com>
2024-09-13 21:17:02 +0700

    add MainActivity

--- app/src/main/java/com/ubaya/projectnmp/MainActivity.kt
+++ app/src/main/java/com/ubaya/projectnmp/MainActivity.kt
@@ -11,6 +11,18 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+        // Example interactions
+        findViewById<ImageView>(R.id.what_we_play_icon).setOnClickListener {
+            Toast.makeText(this, "What We Play Clicked", Toast.LENGTH_SHORT).show()
+        }
+
+        findViewById<ImageView>(R.id.who_we_are_icon).setOnClickListener {
+            Toast.makeText(this, "Who We Are Clicked", Toast.LENGTH_SHORT).show()
+        }
+
+        findViewById<ImageView>(R.id.our_schedule_icon).setOnClickListener {
+            Toast.makeText(this, "Our Schedule Clicked", Toast.LENGTH_SHORT).show()
+        }
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
