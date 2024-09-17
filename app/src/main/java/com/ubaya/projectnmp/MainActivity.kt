f1f67a6c71b48bd50811abd2a1ce57ca08ebda80
DoCopyPaste <docopypaste.user@gmail.com>
2024-09-17 15:47:44 +0700

    Add UI Home & Who We Are + Prepare Buat Page Lain

--- app/src/main/java/com/ubaya/projectnmp/MainActivity.kt
+++ app/src/main/java/com/ubaya/projectnmp/MainActivity.kt
@@ -1,35 +1,42 @@
 package com.ubaya.projectnmp
 
+import android.content.Intent
 import android.os.Bundle
 import android.widget.ImageView
+import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.ubaya.projectnmp.R
+import com.ubaya.projectnmp.WhoWeAreActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-
         setContentView(R.layout.activity_main)
-        // Example interactions
+
+        // Set click listeners
         findViewById<ImageView>(R.id.what_we_play_icon).setOnClickListener {
-                Toast.makeText(this, "What We Play Clicked", Toast.LENGTH_SHORT).show()
+            startActivity(Intent(this, WhatWePlayActivity::class.java))
+        }
+        findViewById<TextView>(R.id.what_we_play_text).setOnClickListener {
+            startActivity(Intent(this, WhatWePlayActivity::class.java))
         }
 
         findViewById<ImageView>(R.id.who_we_are_icon).setOnClickListener {
-            Toast.makeText(this, "Who We Are Clicked", Toast.LENGTH_SHORT).show()
+            startActivity(Intent(this, WhoWeAreActivity::class.java))
+        }
+        findViewById<TextView>(R.id.who_we_are_text).setOnClickListener {
+            startActivity(Intent(this, WhoWeAreActivity::class.java))
         }
 
         findViewById<ImageView>(R.id.our_schedule_icon).setOnClickListener {
-            Toast.makeText(this, "Our Schedule Clicked", Toast.LENGTH_SHORT).show()
+            startActivity(Intent(this, OurScheduleActivity::class.java))
         }
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        findViewById<TextView>(R.id.our_schedule_text).setOnClickListener {
+            startActivity(Intent(this, OurScheduleActivity::class.java))
         }
     }
 }
