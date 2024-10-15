9b85ae276ceb15b08521566fed3f40f4cb2ac3f8
KaisarTomat <s160422040@student.ubaya.ac.id>
2024-10-15 19:56:02 +0700

    team member card

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,5 +52,7 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation ("de.hdodenhof:circleimageview:3.1.0")
     implementation(kotlin("script-runtime"))
 }
