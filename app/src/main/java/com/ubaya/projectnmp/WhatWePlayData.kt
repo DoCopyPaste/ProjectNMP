3250745e7388fc16ffb374b505f8d486c566e995
WhoIsLiLY <127612137+WhoIsLiLY@users.noreply.github.com>
2024-10-16 17:02:25 +0700

    fix some data

--- app/src/main/java/com/ubaya/projectnmp/WhatWePlayData.kt
+++ app/src/main/java/com/ubaya/projectnmp/WhatWePlayData.kt
@@ -7,40 +7,40 @@ object WhatWePlayData {
                 Achievement("Valorant Championship", 2020, "Team A"),
                 Achievement("Valorant Regional Finals", 2021, "Team B"),
                 Achievement("Valorant World Cup", 2022, "Team C"),
-                Achievement("Valorant Invitational", 2023, "Team D"),
+                Achievement("Valorant Invitational", 2022, "Team D"),
                 Achievement("Valorant Pro League", 2024, "Team E"),
             ),
             listOf(
                 Team(
                 "Team A", listOf(
-                    TeamMember("Alice", "Duelist", R.drawable.doraemon),
-                    TeamMember("Bob", "Smoker", R.drawable.ic_launcher_background),
-                    TeamMember("Alice2", "Healer", R.drawable.doraemon),
-                    TeamMember("Bob2", "Support", R.drawable.ic_launcher_background),
+                    TeamMember("Alice", "Duelist", R.drawable.pp),
+                    TeamMember("Bob", "Smoker", R.drawable.pp_1),
+                    TeamMember("Alice2", "Healer", R.drawable.pp_2),
+                    TeamMember("Bob2", "Support", R.drawable.pp_3),
                 )
                 ),
                 Team(
                 "Team B", listOf(
-                    TeamMember("Aaron", "Duelist", R.drawable.doraemon),
-                    TeamMember("Oakley", "Smoker", R.drawable.ic_launcher_background),
-                    TeamMember("Stanley", "Healer", R.drawable.doraemon),
-                    TeamMember("James", "Support", R.drawable.ic_launcher_background),
+                    TeamMember("Aaron", "Duelist", R.drawable.pp),
+                    TeamMember("Oakley", "Smoker", R.drawable.pp_2),
+                    TeamMember("Stanley", "Healer", R.drawable.pp_3),
+                    TeamMember("James", "Support", R.drawable.pp_1),
                 )
                 ),
                 Team(
                 "Team C", listOf(
-                    TeamMember("Darren", "Duelist", R.drawable.doraemon),
-                    TeamMember("Edward", "Smoker", R.drawable.ic_launcher_background),
-                    TeamMember("Randy", "Healer", R.drawable.doraemon),
-                    TeamMember("Ryan", "Support", R.drawable.ic_launcher_background),
+                    TeamMember("Darren", "Duelist", R.drawable.pp_1),
+                    TeamMember("Edward", "Smoker", R.drawable.pp),
+                    TeamMember("Randy", "Healer", R.drawable.pp_2),
+                    TeamMember("Ryan", "Support", R.drawable.pp_3),
                 )
                 ),
                 Team(
                 "Team D", listOf(
-                    TeamMember("Luki", "Duelist", R.drawable.doraemon),
-                    TeamMember("Matthew", "Smoker", R.drawable.ic_launcher_background),
-                    TeamMember("Willy", "Healer", R.drawable.doraemon),
-                    TeamMember("Budi", "Support", R.drawable.ic_launcher_background),
+                    TeamMember("Luki", "Duelist", R.drawable.pp),
+                    TeamMember("Matthew", "Smoker", R.drawable.pp_1),
+                    TeamMember("Willy", "Healer", R.drawable.pp_3),
+                    TeamMember("Budi", "Support", R.drawable.pp_2),
                 )
                 )
             )
@@ -55,34 +55,34 @@ object WhatWePlayData {
                 ), listOf(
                 Team(
                     "Team A", listOf(
-                        TeamMember("Alice", "Duelist", R.drawable.doraemon),
-                        TeamMember("Bob", "Smoker", R.drawable.ic_launcher_background),
-                        TeamMember("Alice2", "Healer", R.drawable.doraemon),
-                        TeamMember("Bob2", "Support", R.drawable.ic_launcher_background)
+                        TeamMember("Alice", "Duelist", R.drawable.pp),
+                        TeamMember("Bob", "Smoker", R.drawable.pp_1),
+                        TeamMember("Alice2", "Healer", R.drawable.pp_2),
+                        TeamMember("Bob2", "Support", R.drawable.pp_3)
                     )
                 ),
                 Team(
                     "Team B", listOf(
-                        TeamMember("Aaron", "Duelist", R.drawable.doraemon),
-                        TeamMember("Oakley", "Smoker", R.drawable.ic_launcher_background),
-                        TeamMember("Stanley", "Healer", R.drawable.doraemon),
-                        TeamMember("James", "Support", R.drawable.ic_launcher_background)
+                        TeamMember("Aaron", "Duelist", R.drawable.pp_1),
+                        TeamMember("Oakley", "Smoker", R.drawable.pp),
+                        TeamMember("Stanley", "Healer", R.drawable.pp_2),
+                        TeamMember("James", "Support", R.drawable.pp_3)
                     )
                 ),
                 Team(
                     "Team C", listOf(
-                        TeamMember("Darren", "Duelist", R.drawable.doraemon),
-                        TeamMember("Edward", "Smoker", R.drawable.ic_launcher_background),
-                        TeamMember("Randy", "Healer", R.drawable.doraemon),
-                        TeamMember("Ryan", "Support", R.drawable.ic_launcher_background),
+                        TeamMember("Darren", "Duelist", R.drawable.pp_3),
+                        TeamMember("Edward", "Smoker", R.drawable.pp_2),
+                        TeamMember("Randy", "Healer", R.drawable.pp_1),
+                        TeamMember("Ryan", "Support", R.drawable.pp),
                     )
                 ),
                 Team(
                     "Team D", listOf(
-                        TeamMember("Luki", "Duelist", R.drawable.doraemon),
-                        TeamMember("Matthew", "Smoker", R.drawable.ic_launcher_background),
-                        TeamMember("Willy", "Healer", R.drawable.doraemon),
-                        TeamMember("Budi", "Support", R.drawable.ic_launcher_background)
+                        TeamMember("Luki", "Duelist", R.drawable.pp_1),
+                        TeamMember("Matthew", "Smoker", R.drawable.pp_2),
+                        TeamMember("Willy", "Healer", R.drawable.pp),
+                        TeamMember("Budi", "Support", R.drawable.pp_3)
                     )
                 )
             )
@@ -97,34 +97,34 @@ object WhatWePlayData {
                 ), listOf(
                 Team(
                 "Team A", listOf(
-                    TeamMember("Alice", "Duelist", R.drawable.doraemon),
-                    TeamMember("Bob", "Smoker", R.drawable.ic_launcher_background),
-                    TeamMember("Alice2", "Healer", R.drawable.doraemon),
-                    TeamMember("Bob2", "Support", R.drawable.ic_launcher_background),
+                    TeamMember("Alice", "Duelist", R.drawable.pp_1),
+                    TeamMember("Bob", "Smoker", R.drawable.pp),
+                    TeamMember("Alice2", "Healer", R.drawable.pp_2),
+                    TeamMember("Bob2", "Support", R.drawable.pp_3),
                 )
                 ),
                 Team(
                 "Team B", listOf(
-                    TeamMember("Aaron", "Duelist", R.drawable.doraemon),
-                    TeamMember("Oakley", "Smoker", R.drawable.ic_launcher_background),
-                    TeamMember("Stanley", "Healer", R.drawable.doraemon),
-                    TeamMember("James", "Support", R.drawable.ic_launcher_background),
+                    TeamMember("Aaron", "Duelist", R.drawable.pp_1),
+                    TeamMember("Oakley", "Smoker", R.drawable.pp),
+                    TeamMember("Stanley", "Healer", R.drawable.pp_2),
+                    TeamMember("James", "Support", R.drawable.pp_3),
                 )
                 ),
                 Team(
                 "Team C", listOf(
-                    TeamMember("Darren", "Duelist", R.drawable.doraemon),
-                    TeamMember("Edward", "Smoker", R.drawable.ic_launcher_background),
-                    TeamMember("Randy", "Healer", R.drawable.doraemon),
-                    TeamMember("Ryan", "Support", R.drawable.ic_launcher_background),
+                    TeamMember("Darren", "Duelist", R.drawable.pp_1),
+                    TeamMember("Edward", "Smoker", R.drawable.pp_2),
+                    TeamMember("Randy", "Healer", R.drawable.pp_3),
+                    TeamMember("Ryan", "Support", R.drawable.pp),
                 )
                 ),
                 Team(
                 "Team D", listOf(
-                    TeamMember("Luki", "Duelist", R.drawable.doraemon),
-                    TeamMember("Matthew", "Smoker", R.drawable.ic_launcher_background),
-                    TeamMember("Willy", "Healer", R.drawable.doraemon),
-                    TeamMember("Budi", "Support", R.drawable.ic_launcher_background),
+                    TeamMember("Luki", "Duelist", R.drawable.pp_1),
+                    TeamMember("Matthew", "Smoker", R.drawable.pp_3),
+                    TeamMember("Willy", "Healer", R.drawable.pp_2),
+                    TeamMember("Budi", "Support", R.drawable.pp),
                 )
                 )
             )
